8e4c91e3b4324c724a45830997b069bfc40aa973
Evgeniy Setrov <evgensetrov@gmail.com>
2025-06-17 23:11:56 +0300

    Cloud Function для сбора данных в s3

---
+++ collecting/index.py
@@ -0,0 +1,51 @@
+import os
+import datetime
+import pytz
+import boto3
+import base64
+
+ACCESS_KEY = os.getenv("ACCESS_KEY")  # key_id из вывода при создании ключа
+SECRET_KEY = os.getenv("SECRET_KEY")  # secret из вывода
+BUCKET_NAME = os.getenv("BUCKET_NAME")  # название бакета, к которому имеет доступ сервисный аккаунт, напр. '17062025-iot-data'
+TIME_ZONE = os.getenv("TIME_ZONE", "Europe/Moscow")
+
+session = boto3.session.Session()
+s3 = session.client(
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        service_name='s3',
+        endpoint_url='https://storage.yandexcloud.net'
+    )
+
+def handler(event, context):
+    try:
+        message = event.get("messages", [])[0]
+        details = message.get("details")
+
+        registry_id = details["registry_id"]
+        device_id = details["device_id"]
+        payload = base64.b64decode(details["payload"]).decode('utf-8')
+
+
+        now = datetime.datetime.now(pytz.timezone(TIME_ZONE))
+        timestamp_str = now.strftime("%Y-%m-%d_%H:%M:%S.%f")[:-3]
+
+        key = f"{registry_id}/{device_id}/{now:%Y-%m/%d}/{timestamp_str}.json"
+
+        s3.put_object(
+            Bucket=BUCKET_NAME,
+            Key=key,
+            Body=payload,
+            ContentType='application/json'
+        )
+
+        return {
+            "statusCode": 200,
+            "body": f"Событие успешно обработано, сообщение положено в {key}"
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": f"Ошибка: {str(e)}\n\nTrigger event: {event}"
+        }
